8dfb24b475a54a22382bec1626f9ddc45d583671
Skitovich Ruslan <zloycvetok@gmail.com>
2020-08-21 21:45:15 +0300

    ДЗ в рублях

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-       int ticketPrice = 105000;   // Последние две цифры это копейки
-       int miles = ticketPrice/2000;
+       int ticketPrice = 1000;   
+       int miles = ticketPrice/20;
        System.out.println(miles);
 
     }
